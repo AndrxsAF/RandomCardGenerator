6391c04a2fd01eacdabf7b04d01e4b1a2a1e9bb5
AndrxsAF <andrxsaf@gmail.com>
2022-01-08 11:52:44 +0100

    feat fixes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-const cardIcon = ["/assets/img/clubs.png", "/assets/img/diamonds.png", "/assets/img/like.png", "/assets/img/spades.png"]
+const cardIcon = ["clubs.png", "diamonds.png", "like.png", "spades.png"]
 const cardNumber = document.querySelector("#number")
 const cardIconTop = document.querySelector("#imgTop")
 const cardIconBtm = document.querySelector("#imgBtm")
@@ -9,7 +9,7 @@ window.onload = function() {
     do {
         iconGenerator = Math.floor(Math.random() * 10)
     } while (iconGenerator > 3)
-    cardIconTop.innerHTML = `<img class="left-img" src="${cardIcon[iconGenerator]}" alt="clubs">`
+    cardIconTop.innerHTML = `<img class="left-img" src="./assets/img/${cardIcon[iconGenerator]}" alt="clubs">`
     cardNumber.innerHTML = `<h1 class="fs-custom">${random}</h1>`
-    cardIconBtm.innerHTML = `<img class="right-img" src="${cardIcon[iconGenerator]}" alt="clubs">`
+    cardIconBtm.innerHTML = `<img class="right-img" src="./assets/img/${cardIcon[iconGenerator]}" alt="clubs">`
 }
